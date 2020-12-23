6839913407de9d10f2f34b7ef5e5c3df4bc54e9e
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 22:21:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#augmented assignment operator
+
+some_value = 5
+
+some_value += 2
+
+print(some_value)
+
+another_value = 5
+
+another_value -= 2
+
+print(another_value)
+
+my_value = 5
+
+my_value *= 2
+
+print(my_value)
+
+your_value = 5
+
+your_value /= 2
+
+print(your_value)
